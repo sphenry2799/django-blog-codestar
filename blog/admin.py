747aedd7d5d8e274a576410fdf39cf8ae7c651fa
sphenry2799 <sp.henry2799@gmail.com>
2025-11-05 19:55:13 +0000

    Add comments to models.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Comment
 
+
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
